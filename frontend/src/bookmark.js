8069b35b9cfc19af9b26681ecc783a76cc9bf26f
junah201 <junah.dev@gmail.com>
2022-12-23 18:19:23 +0900

    feat : 북마크 버튼을 포스트 상단에도 추가 (#36)
    - 블로그 포스트를 상단 블로그명 바로 옆에도 추가하도록 개선함
    - 블로그 버튼 끼리 색깔이 동기화가 안되는 문제가 있음

--- frontend/src/bookmark.js
+++ frontend/src/bookmark.js
@@ -1,9 +1,7 @@
-function add_bookmark_button() {
-	var bookmarkButton = createElement("button", { class: "bookmark-button" });
+function createBookmarkButton() {
+	const bookmarkButton = createElement("button", { class: "bookmark-button" });
 	bookmarkButton.innerHTML =
 		'<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z"/></svg>';
-
-	document.querySelector(".name").append(bookmarkButton);
 
 	bookmarkButton.addEventListener("click", () => {
 		if (bookmarkButton.classList.contains("yellow-bookmark-button")) {
@@ -27,7 +25,6 @@ function add_bookmark_button() {
 			payload: window.location.pathname.split("/")[1].substring(1),
 		},
 		(response) => {
-			console.log(response.is_bookmarked);
 			if (response.is_bookmarked) {
 				bookmarkButton.classList.add("yellow-bookmark-button");
 			} else {
@@ -35,10 +32,23 @@ function add_bookmark_button() {
 			}
 		}
 	);
+
+	return bookmarkButton;
 }
 
-console.log(document.querySelector(".bookmark-button"));
 if (document.querySelector(".bookmark-button") == null) {
-	add_bookmark_button();
-	console.log("SUCCES add bookmark button");
+	var blogProfile = document.querySelector("div > div.name");
+	console.log(blogProfile);
+	if (blogProfile != null && blogProfile != undefined) {
+		var bookmarkButton = createBookmarkButton();
+		blogProfile.append(bookmarkButton);
+		console.log("SUCCES add bookmark button in blog profile");
+	}
+
+	var username = document.querySelector("div.information > span.username");
+	if (username != null && username != undefined) {
+		var bookmarkButton = createBookmarkButton();
+		username.after(bookmarkButton);
+		console.log("SUCCES add bookmark button in top of post");
+	}
 }
