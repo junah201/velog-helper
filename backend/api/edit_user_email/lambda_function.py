a4015bb3693ecf7bc5e808c04d925d111c0bd5fb
junah201 <junah.dev@gmail.com>
2023-04-05 18:48:46 +0900

    feat: Add edit_user_email lambda

---
+++ backend/api/edit_user_email/lambda_function.py
@@ -0,0 +1,37 @@
+import database
+import models
+import json
+from typing import Optional
+
+
+def lambda_handler(event, context):
+    user_id = event.get("pathParameters", {}).get("user_id", None)
+    email = event.get("body", {}).get("email", None)
+
+    if user_id is None:
+        return {
+            'statusCode': 400,
+            'body': json.dumps('Missing user_id')
+        }
+
+    db = next(database.get_db())
+
+    db_user: Optional[models.User] = db.query(
+        models.User).filter(models.User.id == user_id).first()
+
+    if db_user:
+        return {
+            'statusCode': 405,
+            'headers': {'Content-Type': 'application/json; charset=utf-8', 'Access-Control-Allow-Origin': '*'},
+            'body': json.dumps('User already exists')
+        }
+
+    db_user.email = email
+    db.commit()
+    db.refresh(db_user)
+
+    return {
+        'statusCode': 204,
+        'headers': {'Content-Type': 'application/json; charset=utf-8', 'Access-Control-Allow-Origin': '*'},
+        'body': ""
+    }
